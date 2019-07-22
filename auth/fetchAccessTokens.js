5c2b0bb29529aca9ce53f0fd2412e2a4be832920
Zachary Rankin <rankin.zachary@gmail.com>
2019-07-21 19:58:45 -0700

    Simplify promise

--- auth/fetchAccessTokens.js
+++ auth/fetchAccessTokens.js
@@ -10,23 +10,21 @@ module.exports = options => {
   const clientSecret = options.clientSecret;
   const redirectUri = options.redirectUri;
 
-  return new Promise((resolve, reject) => {
-    const url = 'https://www.googleapis.com/oauth2/v4/token';
-    const params = qs.stringify({
-      code,
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uri: redirectUri,
-      grant_type: 'authorization_code',
-    });
-    log.debug('auth/tokens - requesting %s?%s', url, params);
-    axios.post(url, params, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }).then(res => {
-      log.debug('auth/tokens - response %j', res.data);
-      resolve(res.data);
-    }).catch(err => reject(err));
+  const url = 'https://www.googleapis.com/oauth2/v4/token';
+  const params = qs.stringify({
+    code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: 'authorization_code',
+  });
+  log.debug('auth/tokens - requesting %s?%s', url, params);
+  return axios.post(url, params, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  }).then(res => {
+    log.debug('auth/tokens - response %j', res.data);
+    return res.data;
   });
 };
